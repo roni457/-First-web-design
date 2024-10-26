48d7ccdceb61bb42d518934cd546934503ba6744
roni457 <kpes0563@gmail.com>
2024-10-27 00:50:40 +0530

    first commit

---
+++ public/Navbar.jsx
@@ -0,0 +1,55 @@
+import react from 'react'
+        
+const Navbar = () => {
+return (
+<nav>
+
+<div>
+
+{/* logo */}
+
+<div className='max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8'>
+
+<a href="/">    
+
+
+    <img src="/wizardz.jpg" alt="" className='h-8'/>
+    </a>
+
+</div>
+
+
+
+
+
+{/* nav items */}
+<div>
+
+<a href="/">about us</a>
+<a href="/">services</a>
+<a href="/">use cases</a>
+<a href="/">pricing</a>
+<a href="/">request a quote</a>
+</div>
+
+
+/</div>
+
+
+
+
+
+</nav>
+
+
+
+
+
+
+
+
+)
+
+}
+
+export default Navbar
---
+++ style.jsx
@@ -0,0 +1,21 @@
+import './style.css'
+
+function style() {
+
+return (
+
+
+
+    <div className='font-primary'>  
+         
+           <h1>our website</h1> 
+    
+</div>
+
+)
+
+
+
+}
+
+export default style 
